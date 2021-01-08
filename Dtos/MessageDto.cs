5c73715a5456d63518fb3fbf89ce9b5ea5fb4e63
aiger <aigerima.nurlanova@gmail.com>
2021-01-08 23:14:47 +0600

    Добавьте файлы проекта.

---
+++ Dtos/MessageDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleProjectSE2.Dtos
+{
+    public class MessageDto
+    {
+        public string Type { get; set; }
+        public string JsonContent { get; set; }
+    }
+}
